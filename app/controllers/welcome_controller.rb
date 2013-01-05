e05e11188cfb757de391567aadf761c1798aaff5
babaru <babaru.trinit@gmail.com>
2013-01-05 17:08:24 +0800

    today

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,9 +12,9 @@ class WelcomeController < ApplicationController
     client = OAuth2::Client.new('3488938016', 'e97940d12d49fa6e8abf0aa87df6efd6', :site => 'https://api.weibo.com', :authorize_url => '/oauth2/authorize', :token_url => '/oauth2/access_token')
     begin
       token = client.auth_code.get_token(code, :redirect_uri => 'http://conte.sptida.com')
+      Rails.logger.debug token.token
     rescue OAuth2::Error => e
       Rails.logger.debug e.code
     end
-    Rails.logger.debug token.token
   end
 end
