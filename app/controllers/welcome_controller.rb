58d234c0833eb818948068c9521702f38546b2b9
babaru <babaru.trinit@gmail.com>
2013-01-05 23:19:19 +0800

    today

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,7 +10,7 @@ class WelcomeController < ApplicationController
   def authorized
     code = params[:code]
     client = Oauth2::SinaOauth2.new('3488938016', 'e97940d12d49fa6e8abf0aa87df6efd6')
-    token = client.get_token('http://conte.sptida.com')
+    token = client.get_token(code, 'http://conte.sptida.com')
     redirect_to action:index
   end
 end
