d8a488365d724d170cd53526caec8457771ecc75
babaru <babaru.trinit@gmail.com>
2013-01-14 18:12:15 +0800

    today

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,7 +8,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
   belongs_to :account
   attr_accessible :body, :is_published, :published_at, :scheduled_at, :sina_weibo_id, :sina_weibo_url, :title, :type, :user_id, :planet_id, :image, :attachment_access_token, :account_id
-  has_attached_file :image, :styles => { :thumb => "100x100>" }, :path => ":rails_root/public:url", :url => "/system/images/:image_access_token/pic_:style.:extension"
+  has_attached_file :image, :styles => { :thumb => "100x100>" }, :path => ":rails_root/public:url", :url => "/system/images/:attachment_access_token/pic_:style.:extension"
 
   def schedule
     unless self.scheduled_at.nil?
