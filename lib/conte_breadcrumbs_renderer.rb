c1678dec9d4f0c538e7335b171e597e68cc7e449
babaru <babaru.trinit@gmail.com>
2013-01-21 17:38:51 +0800

    modify

--- lib/conte_breadcrumbs_renderer.rb
+++ lib/conte_breadcrumbs_renderer.rb
@@ -1,9 +1,19 @@
-class ConteBreadcrumbsRenderer < SimpleNavigation::Renderer::Breadcrumbs
+class ConteBreadcrumbsRenderer < SimpleNavigation::Renderer::Base
   def render(item_container)
-    contents = a_tags.inject([]) do |list, item|
-      list << content_tag(:li, item.html_safe)
+    a_tag(item_container).join.html_safe
+  end
+
+  protected
+
+  def a_tag(item_container)
+    item_container.items.inject([]) do |list, item|
+      if item.selected?
+        list << content_tag(:li, tag_for(item)) if item.selected?
+        if include_sub_navigation?(item)
+          list.concat a_tags(item.sub_navigation)
+        end
+      end
       list
     end
-    contents.join('').html_safe
   end
 end
